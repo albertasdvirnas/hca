c9d55192b78ddf7bb26f908adeace0400afa010f
Erik Torstensson <nat13eto@student.lu.se>
2020-11-22 14:48:20 +0100

    Update(DL script): Updated the script used for DL analysis

--- src/HCA_Dual_labels.m
+++ src/HCA_Dual_labels.m
@@ -1,5 +1,5 @@
 function [hcaStruct] = HCA_Dual_labels(sets, hcaStruct)
-% HCA_Dual_labels
+%% HCA_Dual_labels
 % Used for comparing fagments of human chromosome to chromosomes using
 % CB (competitive binding) and flourescent markers (dots).
 %
@@ -16,25 +16,25 @@ function [hcaStruct] = HCA_Dual_labels(sets, hcaStruct)
 %
 
 if nargin < 1 % if settings were not provided
-  % import settings
+  %% import settings
   import CBT.Hca.Import.import_hca_settings;
   sets = import_hca_settings('hca_settings.txt');
   assignin('base','sets', sets)
 end
 
 if nargin < 2
-  % Ask for barcodes and import them  
+  %% Ask for barcodes and import them
   import DL.Hca.import_single_timeframe_barcodes
   barcodeGenDense = import_single_timeframe_barcodes(sets);
   if size(barcodeGenDense, 1) > 1
     barcodeGen = barcodeGenDense;
   else
-    sets.kymosets.kymoFile = sets.kymosets.sparseFile;
     import DL.Hca.import_sparse_barcodes
     barcodeGenSparse = import_sparse_barcodes(barcodeGenDense, sets);
     import DL.Hca.merge_barcodegens
     barcodeGen = merge_barcodegens(barcodeGenDense, barcodeGenSparse);
   end
+  assignin('base','barcodeGen', barcodeGen)
   
   
   %% now user theories. They could already be in txt files (if generated
@@ -43,46 +43,30 @@ if nargin < 2
   sets.theoryFileFold = [];
   
   % get user theory
-  import CBT.Hca.Settings.get_user_theory;
-  [theoryStruct, ~] = get_user_theory(sets);
-  [tmpTheoryStruct, ~] = get_user_theory(sets);
-  for i=1:length(theoryStruct)
-    theoryStruct{i}.filename2 = tmpTheoryStruct{i}.filename;
-  end
+  import DL.Hca.get_user_theory_dl;
+  theoryStruct = get_user_theory_dl(sets);
   assignin('base','theoryStruct', theoryStruct)
   
-  % compare theory to experiment
+  %% compare theory to experiment
   import CBT.Hca.Core.Comparison.compare_distance;
-  [rezMax, bestBarStretch, bestLength] = compare_distance( ...
+  rezMax = compare_distance( ...
     barcodeGen, ...
     theoryStruct, ...
     sets);
+  assignin('base','rezMax', rezMax)
   
-  import CBT.Hca.Core.Comparison.combine_theory_results;
-  comparisonStruct = combine_theory_results( ...
+  import DL.Hca.combine_theory_results_dl;
+  comparisonStruct = combine_theory_results_dl( ...
     theoryStruct, ...
-    rezMax, ...
-    bestBarStretch, ...
-    bestLength);
+    rezMax);
   assignin('base','comparisonStruct', comparisonStruct)
   
   import DL.Hca.get_display_results_dl;
   get_display_results_dl( ...
     barcodeGen, ...
-    struct(), ...
     comparisonStruct, ...
     theoryStruct, ...
     sets);
-  
-%   import CBT.Hca.Core.additional_computations
-%   additional_computations( ...
-%     barcodeGen, ...
-%     struct(), ...
-%     comparisonStruct, ...
-%     theoryStruct, ...
-%     comparisonStructAll, ...
-%     sets);
-  
   
   
 else
