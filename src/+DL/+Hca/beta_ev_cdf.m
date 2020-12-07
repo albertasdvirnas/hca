e22bbc4af66e2d5c754f9bbe10314a46e0d5e56c
Erik Torstensson <nat13eto@student.lu.se>
2020-12-07 18:39:41 +0100

    Tweak(pcc p-val): Small change, big boost in speed

--- src/+DL/+Hca/beta_ev_cdf.m
+++ src/+DL/+Hca/beta_ev_cdf.m
@@ -18,10 +18,10 @@ if nargin < 5 || ~extraPrecision
   end
 else
   if length(a) > 1
-    p = mean(cell2mat(arrayfun(@(a1) (0.5*(vpa(2) - sign(x).*vpa(betainc(x.^2, 0.5*b, a1/2-1, 'upper')))).^n, a, 'un', 0)), 2);
+    p = mean(cell2mat(arrayfun(@(a1) (0.5*(vpa(2) - sign(x).*betainc(x.^2, 0.5*b, a1/2-1, 'upper'))).^n, a, 'un', 0)), 2);
     p = reshape(p, xs);
   else
-    p = (0.5*(vpa(2) - sign(x).*vpa(betainc(x.^2, 0.5*b, a/2-1, 'upper')))).^n;
+    p = (0.5*(vpa(2) - sign(x).*betainc(x.^2, 0.5*b, a/2-1, 'upper'))).^n;
   end
   p = min(p, vpa(1)-10^-digits);
 end
