d527b06d6c70b70798231318e8c49ffffb98354d
Erik Torstensson <nat13eto@student.lu.se>
2020-12-21 11:40:19 +0100

    tweak(p-value): Minor optimization to cdf

--- src/+DL/+Hca/trunc_normal_ev_cdf.m
+++ src/+DL/+Hca/trunc_normal_ev_cdf.m
@@ -1,5 +1,4 @@
 function p = trunc_normal_ev_cdf(x, m, s, n, a, b, extraPrecision)
-if s < 0; p = nan; return; end
 if nargin < 5
   a = 0;
 end
@@ -15,29 +14,30 @@ if length(m) > 1 || length(s) > 1
 end
 
 eta = (x - m)./s;
-alpha = (a - m)./s;
-beta = (b - m)./s;
+alph = (a - m)./s;
+bet = (b - m)./s;
 
 ncdf = @(x) .5*(1+erf(x/sqrt(2)));
-vpa_ncdf = @(x) .5*(vpa(1)+erf(vpa(x/sqrt(2))));
+vpa_ncdf = @(x) .5*(vpa(2)-erfc(x/sqrt(2)));
+alph2 = ncdf(alph);
+bet2 = ncdf(bet);
 
 if nargin < 7 || ~extraPrecision
   if length(m) > 1 || length(s) > 1
-    p = mean(((ncdf(eta) - ncdf(alpha))./(ncdf(beta) - ncdf(alpha))).^n, 2);
+    p = mean(((ncdf(eta) - alph2)./(bet2 - alph2)).^n, 2);
     p = reshape(p, xs);
   else
-    p = ((ncdf(eta) - ncdf(alpha))/(ncdf(beta) - ncdf(alpha))).^n;
+    p = ((ncdf(eta) - alph2)/(bet2 - alph2)).^n;
   end
 else
   if length(m) > 1 || length(s) > 1
     % This operation is extremely slow using vpa :(
-    tmpA = repmat(vpa_ncdf(alpha), xs(1), 1);
-    tmpB = repmat(vpa_ncdf(beta), xs(1), 1);
-    p = mean(((vpa_ncdf(eta) - tmpA)./(tmpB - tmpA)).^n, 2);
+    bet3 = repmat(bet2 - alph2, length(x), 1);
+    alph3 = alph2./bet3;
+    p = mean((vpa_ncdf(eta)./bet3 - alph3).^n, 2);
     p = reshape(p, xs);
   else
-    p = ((vpa_ncdf(eta) - vpa_ncdf(alpha))/(vpa_ncdf(beta) - vpa_ncdf(alpha))).^n;
+    p = ((vpa_ncdf(eta) - alph2)/(bet2 - alph2)).^n;
   end
   p = min(p, vpa(1)-10^-digits);
 end
-
