7a437e99acd7c14e5721fe91d74f848ab0637702
Erik Torstensson <nat13eto@student.lu.se>
2020-12-21 11:41:34 +0100

    feat(barcode import): Dot theory from set path

--- src/+DL/+Hca/get_user_theory_dl.m
+++ src/+DL/+Hca/get_user_theory_dl.m
@@ -14,53 +14,74 @@ tmpTheoryStruct = cell(1,2);
 btype = {'CB', 'Dots'};
 
 for i=1:2
-  % loads figure window
-  import Fancy.UI.Templates.create_figure_window;
-  [hMenuParent, tsHCA] = create_figure_window([btype{i} ' HCA theory selection tool'],'HCA');
-  
-  import Fancy.UI.Templates.create_import_tab;
-  cache = create_import_tab(hMenuParent, tsHCA, [btype{i} '-theory']);
-  uiwait(gcf);
-  
-  dd = cache('selectedItems');
-  sets.theoryFile = dd(1:end/2);
-  sets.theoryFileFold = dd((end/2+1):end);
-  
-  % Sort by name, hopefully both barcode types have the same order....
-  [sets.theoryFile, sortedIds] = sort(sets.theoryFile);
-  sets.theoryFileFold = sets.theoryFileFold(sortedIds);
-  
-  delete(hMenuParent);
-  
-  if i==2
-    answer = questdlg('What is the second type of theory?', ...
-      'Choose label type', ...
-      'Intensity profile', ...
-      'Dots in pixels', ...
-      'Intensity profile');
-    if strcmp(answer, 'Dots in pixels')
-      tmpTheoryStruct{i} = cell(1, length(sets.theoryFile));
-      for j=1:length(sets.theoryFile)
-        tmpTheoryStruct{i}{j}.filename = fullfile(sets.theoryFileFold{j}, ...
-          sets.theoryFile{j});
+  if not(sets.theory.askfortheory)
+    try
+      switch i
+        case 1
+          fid = fopen(sets.theories);
+        case 2
+          fid = fopen(sets.sparseTheories);
       end
-      break
+      thrNames = textscan(fid,'%s','delimiter','\n');
+      fclose(fid);
+      for j=1:length(thrNames{1})
+        [FILEPATH,NAME,EXT] = fileparts(thrNames{1}{j});
+        sets.theoryFile{j} = strcat(NAME,EXT);
+        sets.theoryFileFold{j} = FILEPATH;
+      end
+    catch
+      sets.theory.askfortheory = 1;
     end
   end
+  if sets.theory.askfortheory
+    % loads figure window
+    import Fancy.UI.Templates.create_figure_window;
+    [hMenuParent, tsHCA] = create_figure_window([btype{i} ' HCA theory selection tool'],'HCA');
     
+    import Fancy.UI.Templates.create_import_tab;
+    cache = create_import_tab(hMenuParent, tsHCA, [btype{i} '-theory']);
+    uiwait(gcf);
+    
+    dd = cache('selectedItems');
+    sets.theoryFile = dd(1:end/2);
+    sets.theoryFileFold = dd((end/2+1):end);
+    
+    % Sort by name, hopefully both barcode types have the same order....
+    [sets.theoryFile, sortedIds] = sort(sets.theoryFile);
+    sets.theoryFileFold = sets.theoryFileFold(sortedIds);
+    
+    delete(hMenuParent);
+    
+    if i==2
+      answer = questdlg('What is the second type of theory?', ...
+        'Choose label type', ...
+        'Intensity profile', ...
+        'Dots in pixels', ...
+        'Intensity profile');
+      if strcmp(answer, 'Dots in pixels')
+        tmpTheoryStruct{i} = cell(1, length(sets.theoryFile));
+        for j=1:length(sets.theoryFile)
+          tmpTheoryStruct{i}{j}.filename = fullfile(sets.theoryFileFold{j}, ...
+            sets.theoryFile{j});
+        end
+        break
+      end
+    end
+  end
+  
   % now load theory
   import CBT.Hca.UI.Helper.load_theory;
   tmpTheoryStruct{i} = load_theory(sets);
 end
 
-assignin('base', 'tmpTheoryStruct', tmpTheoryStruct)
+% assignin('base', 'tmpTheoryStruct', tmpTheoryStruct)
 
 if not(length(tmpTheoryStruct{1}) == length(tmpTheoryStruct{2}))
   throw(MException('theoryImport:empty', ...
     'Theory pair member is empty, aborting theory import.'));
 end
 
-if strcmp(answer, 'Dots in pixels')
+if sets.theory.askfortheory && strcmp(answer, 'Dots in pixels')
   import Microscopy.Simulate.Core.apply_point_spread_function
   for j=1:length(tmpTheoryStruct{1})
     thisDots = importdata(tmpTheoryStruct{2}{j}.filename);
@@ -75,12 +96,13 @@ if strcmp(answer, 'Dots in pixels')
     fd = fopen(newPath,'w');
     fprintf(fd, strcat([' %5.' num2str(sets.theory.precision) 'f ']), dotbar);
     fclose(fd);
-    theoryStruct{j} = tmpTheoryStruct{1}{j};
-    theoryStruct{j}.filename2 = newPath;
+    tmpTheoryStruct{2}{j}.filename = newPath;
   end
-else
-  for j=1:length(tmpTheoryStruct{1})
-    theoryStruct{j} = tmpTheoryStruct{1}{j};
-    theoryStruct{j}.filename2 = tmpTheoryStruct{2}{j}.filename;
-  end
+end
+% import CBT.Hca.Core.Analysis.convert_nm_ratio;
+% tmpTheoryStruct{1} = convert_nm_ratio(sets.theory.nmbp, tmpTheoryStruct{1}, sets);
+% tmpTheoryStruct{2} = convert_nm_ratio(sets.theory.nmbp, tmpTheoryStruct{2}, sets);
+for j=1:length(tmpTheoryStruct{1})
+  theoryStruct{j} = tmpTheoryStruct{1}{j};
+  theoryStruct{j}.filename2 = tmpTheoryStruct{2}{j}.filename;
 end
